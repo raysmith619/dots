1f9b1d8255d4e9010451e22d6f2c0ce8f51e5198
Charles R Smith (Ray) <raysmith@alum.mit.edu>
2020-02-26 18:18:37 -0500

    Create first dots demo bat file

--- bat/dots.bat
+++ bat/dots.bat
@@ -1,1 +1,9 @@
-
+set GAMES_PATH=c:\users\raysm\GamesDemo
+set PROJ_MODULE=dots
+set PROJ_PATH=%GAMES_PATH%\%PROJ_MODULE
+set COMPILER=C:\ProgramData\Anaconda3\envs\py36
+set RESOURCE_LIB=%GAMES_PATH%\resource_lib\src
+set PYTHONPATH=%PYTHONPATH%;%RESOURCE_LIB%
+%COMPILER%\python src\crs_dots.py ^
+  --trace execute,keycmd,execute_part_change,show_move ^
+  --show_score=False --stroke_move=False --playing=G,Ax,Av,D
