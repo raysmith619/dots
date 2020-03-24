88a29ae96b8f90b76d4cc679889196ff1c6c5ed2
Charles R Smith (Ray) <raysmith@alum.mit.edu>
2020-03-23 23:09:13 -0400

    Example playable snapshot files

---
+++ gm_snaps/one_square_SN_003.py
@@ -0,0 +1,13 @@
+# C:\Users\raysm\workspace\python\crs_dots\gm_snaps\one_square_SN_003.py
+# On: March 22, 2020
+
+from dots_commands import *
+
+set_playing("Gp,Gm")
+set_play(label_name="Grampy", name="Grampy", label="Gp", playing=True, 
+        position=1, color="blue", color_bg="light blue", voice=False, 
+        help_play=False, pause=0.0, auto=False, level=2, steven=0.0)
+set_play(label_name="Grammy", name="Grammy", label="Gm", playing=True, 
+        position=2, color="red", color_bg="light yellow", voice=False, 
+        help_play=False, pause=0.0, auto=True, level=2, steven=0.0)
+start_game()
